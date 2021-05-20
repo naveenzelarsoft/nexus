7a8f7bd912d10b5b4d160c492145a9ae787dd86a
naveen <naveen@gmail.com>
2021-05-20 16:33:33 +0530

    script

--- nexus.groovy
+++ nexus.groovy
@@ -1,4 +1,4 @@
-def {
+def nexus() {
     command = "curl -f -v -u admin:admin --upload-file users.zip http://3.208.90.51:8081/repository/users/users.zip"
     dif execute_state=sh(returnStdout: true, script: command)
 }

99b0afd7314e6482e4ca7fea2bff1beae5523d08
naveen <naveen@gmail.com>
2021-05-22 12:33:19 +0530

    removed go lang dependencies

--- vars/todos.groovy
+++ vars/todos.groovy
@@ -19,23 +19,6 @@ def call(Map params = [:]) {
             APP_TYPE = "${args.APP_TYPE}"
         }
         stages {
-            stage('Get Dependencies') {
-                when {
-                    environment name: 'APP_TYPE', value: 'GOLANG'
-                }
-
-                steps {
-                    sh '''
-          go get github.com/dgrijalva/jwt-go
-          go get github.com/labstack/echo
-          go get github.com/labstack/echo/middleware
-          go get github.com/labstack/gommon/log
-          go get github.com/openzipkin/zipkin-go
-          go get github.com/openzipkin/zipkin-go/middleware/http
-          go get github.com/openzipkin/zipkin-go/reporter/http
-          '''
-                }
-            }
 
             stage('Build Project') {
                 steps {

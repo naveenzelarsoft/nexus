15d2701c43a65da6ee46b4fe09c7465444c9b99e
naveen <naveen@gmail.com>
2021-05-24 11:29:01 +0530

    removed .zip

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -17,7 +17,7 @@ def make_artifacts (APP_TYPE , COMPONENT) {
         print execute_com
     }
     else if (APP_TYPE == "GOLANG") {
-        command = "zip -r ${FILENAME}.zip Login"
+        command = "zip -r ${FILENAME} Login"
         def execute_com = sh(returnnStdout: true, script: command)
         print execute_com
     }

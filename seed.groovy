4a2b8f307e6d8ccb7178a37acb09b268615f0ede
naveen <naveen@gmail.com>
2021-06-18 12:59:56 +0530

    jenkins-deploy

--- seed.groovy
+++ seed.groovy
@@ -31,3 +31,26 @@ for (i in 0..count) {
         }
     }
 }
+
+
+
+pipelineJob("Deployment Pipeline") {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/naveenzelarsoft/nexus.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('main')
+                    }
+                }
+            }
+            'scriptPath'('jenkins-deployment')
+            'lightweight'(true)
+        }
+    }
+}

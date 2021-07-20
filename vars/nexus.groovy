6c26b5a0092a13a23290d4ff57be3b2fab06c9e0
naveen <naveen@gmail.com>
2021-07-20 12:22:58 +0530

    nexusip

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -3,7 +3,7 @@ def nexus(COMPONENT) {
     def get_branch_exec=sh(returnStdout: true, script: get_branch)
     def FILENAME=COMPONENT+'-'+get_branch_exec+'.zip'
 
-    command = "curl -f -v -u admin:admin --upload-file ${FILENAME} http://${NEXUS_IP}:8081/repository/${COMPONENT}/${FILENAME}"
+    command = "curl -f -v -u admin:admin --upload-file ${FILENAME} http://3.238.102.240:8081/repository/${COMPONENT}/${FILENAME}"
     def execute_state=sh(returnStdout: true, script: command)
 }
 
